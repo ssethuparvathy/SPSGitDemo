45d9c9224fe88e320a15903dec1776d544c245eb
Sethu <sethuparvathysubhash@gmail.com>
2022-03-29 19:42:35 +0530

    second commit

---
+++ mavensetup/src/test/java/sethulearning/mavensetup/BusinesslogicfiveTest.java
@@ -0,0 +1,22 @@
+package sethulearning.mavensetup;
+
+import org.testng.annotations.Test;
+
+public class BusinesslogicfiveTest {
+	
+	
+	
+	
+	@Test(groups="Smoke")
+	public void testMethod9()
+	{
+		System.out.println("Test Method nine logic executed");
+	}
+	
+	@Test
+	public void testMethod10()
+	{
+		System.out.println("Test Method ten logic executed");
+	}
+
+}
